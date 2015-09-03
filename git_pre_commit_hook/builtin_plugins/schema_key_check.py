a06b6222714fd861474aad976e470cecedb2356c
Adrian Maceiras <amaceiras@rmn.com>
2015-09-03 11:37:27 -0500

    [NOTICKET] Add pre-commit check to validate schema keys

---
+++ git_pre_commit_hook/builtin_plugins/schema_key_check.py
@@ -0,0 +1,28 @@
+"""Check that files contains valid JSON."""
+import os
+import fnmatch
+import json
+
+
+DEFAULTS = {
+    'files': '*.json',
+}
+
+def check(file_staged_for_commit, options):
+    basename = os.path.basename(file_staged_for_commit.path)
+    if not fnmatch.fnmatch(basename, options.json_files):
+        return True
+    try:
+        content = json.loads(file_staged_for_commit.contents)
+        title = content['title']
+        path, ext = os.path.splitext(file_staged_for_commit.path)
+        idx = path.find("/project/")
+        if idx == -1:
+            return False
+        path = path[idx + len("/project/"):]
+        path = path.replace("/", ".")
+        assert path == title
+    except:
+        return False
+    else:
+        return True
